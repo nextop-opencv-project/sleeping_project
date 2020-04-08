4860479c8f3ccd0847631bd1d4cc7daf4ffb27e5
Dongwon9 <55753621+Dongwon9@users.noreply.github.com>
2020-04-08 20:29:24 +0900

    Update sleepdetect.py

--- sleepdetect.py
+++ sleepdetect.py
@@ -4,9 +4,6 @@ import cv2
 from imutils import face_utils
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector=dlib.get_frontal_face_detector()
-
-
-
 #Eye Aspect Ratio: 눈의 모양을 보고 눈이 얼마나 감겨있는지 측정한다
 def EAR(eye):
     A=dist.euclidean(eye[1],eye[5])
@@ -15,8 +12,7 @@ def EAR(eye):
     ear=(A+B)/(2.0*C)
     return ear
 
-while(True):
-    
+while(True):    
     #image=cv2.VideoCapture(0)  # <-웹캠에서 지금 순간의 이미지를 가져오는 코드
     image = cv2.imread("human.jpg", cv2.IMREAD_GRAYSCALE)#<-웹캠이 없으니 이미지로 대체
     rects = detector(image,0)
