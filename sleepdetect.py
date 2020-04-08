1ecca1c3a1adfbf27daeaf8c283dcc708d1d1239
Dongwon9 <55753621+Dongwon9@users.noreply.github.com>
2020-04-08 20:17:52 +0900

    Update sleepdetect.py

--- sleepdetect.py
+++ sleepdetect.py
@@ -4,7 +4,7 @@ import cv2
 from imutils import face_utils
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector=dlib.get_frontal_face_detector()
-#frame=cv2.VideoCapture(0)
+
 
 
 #Eye Aspect Ratio: 눈의 모양을 보고 눈이 얼마나 감겨있는지 측정한다
@@ -15,16 +15,18 @@ def EAR(eye):
     ear=(A+B)/(2.0*C)
     return ear
 
-#임시로 사람 얼굴 이미지를 사용함
-image = cv2.imread("human.jpg", cv2.IMREAD_GRAYSCALE)
-rects = detector(image,0)
-for(i, rect) in enumerate(rects):
-
-    shape = predictor(image,rect)
-    shape = face_utils.shape_to_np(shape)
-    #얼굴의 특징점 찾기
-    print(shape[36:42])
-    ear_left = EAR(shape[36:42])
-    ear_right = EAR(shape[42:48])
-    print(ear_left)
-    print(ear_right)
+while(True):
+    
+    #image=cv2.VideoCapture(0)  # <-웹캠에서 지금 순간의 이미지를 가져오는 코드
+    image = cv2.imread("human.jpg", cv2.IMREAD_GRAYSCALE)#<-웹캠이 없으니 이미지로 대체
+    rects = detector(image,0)
+    for(i, rect) in enumerate(rects):
+        shape = predictor(image,rect)
+        shape = face_utils.shape_to_np(shape)
+        #얼굴의 특징점 찾기
+        print(shape[36:42])
+        ear_left = EAR(shape[36:42])
+        ear_right = EAR(shape[42:48])
+        print(ear_left)
+        print(ear_right)
+    
