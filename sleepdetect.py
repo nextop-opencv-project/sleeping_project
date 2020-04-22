f71bec604ed28192ca2b523e3f0f85e9b8a81eda
Dongwon9 <55753621+Dongwon9@users.noreply.github.com>
2020-04-22 21:17:26 +0900

    Update sleepdetect.py

--- sleepdetect.py
+++ sleepdetect.py
@@ -1,7 +1,7 @@
 import dlib
 import cv2
 from imutils import face_utils
-import ourmodulepack as m # 이 파일은 따로 업로드 했으니 따로 확인하시오
+import ourmodulepack as m
 import keyboard
 import sys
 from playsound import playsound
@@ -14,31 +14,33 @@ counter = 0
 sleeping = False
 FRAMES_PER_SECOND = m.fps_calculate()
 COUNTER_THRESHOLD = SLEEPTIME_THRESHOLD * FRAMES_PER_SECOND
-
+if FRAMES_PER_SECOND == -1:
+    print("초당 프레임률 계산 실패!")
 camera = cv2.VideoCapture(0)
 if not camera.isOpened():
     sys.exit("카메라가 감지되지 않았습니다!")
 
 
 while True:
-    image = camera.read()  # <-웹캠에서 지금 순간의 이미지를 가져오는 코드
-    # image = cv2.imread("human.jpg", cv2.IMREAD_GRAYSCALE)  # <-웹캠이 없으니 이미지로 대체
+    # 1-1
+    # image = camera.read()
+    image = cv2.imread("human.jpg", cv2.IMREAD_GRAYSCALE)
+    # 1-2?
     rects = detector(image, 0)
     for (i, rect) in enumerate(rects):
         shape = predictor(image, rect)
         shape = face_utils.shape_to_np(shape)
-        # 얼굴의 특징점 찾기
-        print(shape[36:42])
-        # 두 눈의 EAR 값을 계산, 평균값을 구한다.
-        ear_left = m.EAR(shape[36:42])
-        ear_right = m.EAR(shape[42:48])
+        # 2-1에 해당하는 코드가...없었다
+        # 2-2
+        ear_left = m.EAR(shape[36:42])  # 왼쪽눈
+        ear_right = m.EAR(shape[42:48])  # 오른쪽눈
         average_ear = (ear_left + ear_right) / 2
         # EAR 값이 기준보다 높으면 카운터 증가
-        if average_ear >= EAR_THRESHOLD:
+        if average_ear <= EAR_THRESHOLD:
             counter += 1
         elif counter > 0:
             counter -= 1
-        # 카운터가 기준값을 넘으면 경고 출력
+        # 카운터가 기준값을 넘으면, 알람 소리를 재생한다.
         if counter >= COUNTER_THRESHOLD:
             sleeping = True
         else:
@@ -46,5 +48,6 @@ while True:
         if sleeping:
             print("졸음 경고!!")
             playsound('alarm.mp3')
-        if keyboard.is_pressed('q'):  # 'q'를 누르면 종료
-            break
+    if keyboard.is_pressed('q'):  # 'q'를 누르면 종료
+        break
+camera.release()
